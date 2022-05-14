f0f68222bbdcaf0881c067c1ea16c27e2658f2e8
tubi55 <tubi55@nate.com>
2022-05-14 11:47:13 +0900

    header, footer styleing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import Header from './Header';
 import Footer from './Footer';
-import Content from './Content';
+import './scss/style.scss';
 
-//컴포넌트 함수의 JSX를 생성하는 리턴문 안쪽에서는 연산이 필요한 모든 코드를 중괄호로 감싸줌 (주석)
-
-function App(){
+function App() {
 	return (
 		<>
 			<Header />
-			{/* <Content /> */}
-			<Footer />			
+			<Footer />
 		</>
-	)
+	);
 }
 
 export default App;
-
-
