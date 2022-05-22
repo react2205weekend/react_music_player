88c96e6ba5391c551b2c0f2fd97c6c0f9c782a97
tubi55 <tubi55@nate.com>
2022-05-22 10:11:01 +0900

    좌우버튼 이벤트 연결

--- src/Section.js
+++ src/Section.js
@@ -1,4 +1,5 @@
 import Card from './Card';
+import { useState } from 'react';
 
 function Section() {
 	const arr = [
@@ -11,13 +12,23 @@ function Section() {
 		'Seasonal',
 		'Vespers',
 	];
+	const [count, setCount] = useState(0);
 
 	return (
-		<section style={{ top: '140%' }}>
-			{arr.map((item, idx) => {
-				return <Card key={idx} item={item} index={idx} num={arr.length} />;
-			})}
-		</section>
+		<>
+			<section
+				style={{
+					top: '140%',
+					transform: `rotate(${(360 / arr.length) * count}deg)`,
+				}}>
+				{arr.map((item, idx) => {
+					return <Card key={idx} item={item} index={idx} num={arr.length} />;
+				})}
+			</section>
+
+			<a href='#' className='prev' onClick={() => setCount(count - 1)}></a>
+			<a href='#' className='next' onClick={() => setCount(count + 1)}></a>
+		</>
 	);
 }
 
